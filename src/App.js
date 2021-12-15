90cb0561e8dd5cb612d1aa53e7009148d1299351
choi jiwoo <cho2.jiwoo@gmail.com>
2021-12-16 01:48:04 +0900

    feat: add routes to different pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './component/page/Home';
+import About from './component/page/About';
+import Search from './component/page/Search';
+import Stay from './component/page/Stay';
+import Activity from './component/page/Activity';
+import Plogging from './component/page/Plogging';
 
 function App() {
-  return ();
+  return (
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+
+          <Route path='/cafe' exact component={Search} />
+          <Route path='/restaurant' exact component={Search} />
+          <Route path='/stay' exact component={Stay} />
+          <Route path='/activity' exact component={Activity} />
+          <Route path='/plogging' exact component={Plogging} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
