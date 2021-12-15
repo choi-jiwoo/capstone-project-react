7e1750ce420e247036bb8a96f3c88d506b8d86d7
choi jiwoo <cho2.jiwoo@gmail.com>
2021-12-16 01:57:38 +0900

    Add text inside hero image

--- src/component/page/Home.js
+++ src/component/page/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import banner from '../../asset/main_banner.jpg';
 
 function Home() {
@@ -10,6 +11,18 @@ function Home() {
         style={{ height: '700px' }}
         alt='banner'
       />
+      <div className='relative pt-48 text-center'>
+        <p className='text-2xl font-bold'>세상을 바꾸는 여행</p>
+        <p className='text-4xl font-bold mb-2'>지금 내가 시작합니다</p>
+        <p className='text-xl pb-4'>
+          여행자에게는 배려와 감동을, 여행지역에는 환경보전과 경제에 기여하는
+          여행!
+        </p>
+
+        <button className='bg-white font-bold rounded-full py-3 px-6'>
+          <Link to='/about'>더 알아보기</Link>
+        </button>
+      </div>
     </>
   );
 }
