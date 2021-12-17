3a5ab6219391b9279fe22fc6fd01855b7a551f34
choi jiwoo <cho2.jiwoo@gmail.com>
2021-12-18 01:48:24 +0900

    Add plogging page and links to gpx path

--- src/component/page/Plogging.js
+++ src/component/page/Plogging.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import Gpx from '../Gpx';
+import Gpx from './Gpx';
+import { Link } from 'react-router-dom';
+import courseList from '../../asset/courseList.json';
 
 function Plogging() {
-  return <Gpx />;
+  console.log(courseList);
+  const CourseBtn = ({ courseName }) => (
+    <button type='button' className='mx-2 my-3'>
+      <Link
+        to={{
+          pathname: '/plogging/courseView',
+          state: {
+            courseName: courseName,
+          },
+        }}
+        className='border-2 rounded-lg border-black hover:border-blue-600 p-2 '
+      >
+        {courseName}
+      </Link>
+    </button>
+  );
+
+  const renderBtn = () => {
+    return courseList.map((data) => (
+      <CourseBtn key={data.id} courseName={data.courseName} />
+    ));
+  };
+
+  return <div className='m-3'>{renderBtn()}</div>;
 }
 
 export default Plogging;
