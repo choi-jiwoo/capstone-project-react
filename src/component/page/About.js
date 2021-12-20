4d4dff5d54abf924e7281a0c2a4bed3751592875
choi jiwoo <cho2.jiwoo@gmail.com>
2021-12-21 00:28:31 +0900

    Change text block style to justify

--- src/component/page/About.js
+++ src/component/page/About.js
@@ -25,7 +25,7 @@ function About() {
             <div className='flex flex-col py-8 px-4 shadow-sm bg-green-100'>
               <p className='text-2xl'>책임 관광</p>
               <p className='pb-3'>Responsible Tourism</p>
-              <p>
+              <p className='text-justify'>
                 현지의 자연과 문화 자원을 비롯하여 관광과 관련된 모든 분야에
                 대한 이익을 존중하는 관광
               </p>
@@ -33,7 +33,7 @@ function About() {
             <div className='flex flex-col py-8 px-4 shadow-sm bg-green-100'>
               <p className='text-2xl'>생태 관광</p>
               <p className='pb-3'>Eco Tourism</p>
-              <p>
+              <p className='text-justify'>
                 자연 지역에서 책임있는 관광객의 태도와 행동을 통하여 이루어져
                 행해지는 관광행위이며, 지역주민의 참여 및 이익 창출이 동반되어
                 관광에 의한 환경 및 사회 문화적 영향을 관리하는 관광
@@ -42,7 +42,7 @@ function About() {
             <div className='flex flex-col py-8 px-4 shadow-sm bg-green-100'>
               <p className='text-2xl'>지속 가능한 관광</p>
               <p className='pb-3'>Sustainable Tourism</p>
-              <p>
+              <p className='text-justify'>
                 문화의 보전, 필수적인 생태적 과정, 생물 다양성, 경제적, 사회적,
                 심미적 필요를 총족시킬 수 있도록 모든 자원을 관리하는 것,
                 경제적, 환경적, 그리고 사회문화적 지속가능성 등 3개 차원을 모두
@@ -56,7 +56,7 @@ function About() {
         <div className='text-3xl text-center py-4 font-bold'>플로깅이란</div>
         <div className='grid grid-cols-2 mb-20'>
           <img className='ml-12 rounded-xl' src={ploggingImg} alt='plogging' />
-          <div className='text-xl px-12 p-14'>
+          <div className='text-xl px-12 p-14 text-justify'>
             <div className='mb-10'>
               플로깅(plogging)은 ‘이삭을 줍는다’는 뜻의 스웨덴어 ‘Plocka upp’과
               ‘달리는 운동’을 뜻하는 영어 ‘Jogging’의 합성어로
