d5fe3138a1bcbf20dd995ee526fbae8fd9d3ee48
choi jiwoo <cho2.jiwoo@gmail.com>
2021-12-21 01:03:15 +0900

    Redesign footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import footer1 from '../asset/footer_logo.gif';
 
 function Footer() {
+  const courseUrl = 'https://biz.ajou.ac.kr/ebiz/index.jsp';
+
   return (
-    <footer className='static bottom-0 w-full p-8 bg-light text-center'>
-      <div className='grid grid-cols-2'>
-        <div className='flex flex-col'>
-          <div className='font-bold'>Footer1</div>
-          <div>item1</div>
-          <div>item2</div>
+    <footer className='static bottom-0 bg-white text-center m-6 mx-10'>
+      <div className='grid grid-cols-3 text-gray-400 text-sm p-6'>
+        <div className='pr-12'>
+          <img src={footer1} alt='footer_logo' />
         </div>
-        <div className='flex flex-col'>
-          <div className='font-bold'>Footer2</div>
-          <div>item1</div>
-          <div>item2</div>
+        <div className='flex flex-col text-center'>
+          <a
+            href={courseUrl}
+            className='hover:text-gray-600 font-bold tracking-widest'
+          >
+            아주대학교 경영대학 e비즈니스학과
+          </a>
+          <div className='text-xs'>
+            21-2 비즈니스 애널리틱스 (캡스톤 디자인)
+          </div>
+        </div>
+        <div className='flex gap-2 flex-auto justify-end self-center'>
+          <span className='font-bold tracking-widest'>TEAM</span>
+          <span>김선태</span>
+          <span>백세희</span>
+          <span>장예슬</span>
+          <span>최지우</span>
         </div>
       </div>
     </footer>
