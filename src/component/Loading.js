4b757264eb107bb847822da7bd2bb862f8bbf7b4
choi jiwoo <cho2.jiwoo@gmail.com>
2021-12-22 20:36:39 +0900

    Set full width to loading component

--- src/component/Loading.js
+++ src/component/Loading.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Loading({ loading }) {
   let loader = (
-    <button className='border-transparent p-4 mx-auto' disabled>
+    <button className='w-full border-transparent p-4 mx-auto' disabled>
       <div className='animate-pulse flex space-x-4 justify-center'>
         <div className='text-blue-400 font-semibold'>Processing</div>
       </div>
