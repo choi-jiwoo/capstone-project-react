da5a31036181b5eff2801005c2dc591637479418
choi jiwoo <cho2.jiwoo@gmail.com>
2021-12-23 22:52:27 +0900

    Add scrolling to top when routing to a page

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import Footer from './component/Footer';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './component/page/Home';
 import About from './component/page/About';
-import Search from './component/page/Search';
+import Place from './component/page/Place';
 import Stay from './component/page/Stay';
-import Activity from './component/page/Activity';
 import Plogging from './component/page/Plogging';
 import Gpx from './component/page/Gpx';
+import District from './component/page/District';
+import ScrollToTop from './component/scrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className='App'>
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/about' component={About} />
 
-          <Route path='/cafe' exact component={Search} />
-          <Route path='/restaurant' exact component={Search} />
+          <Route path='/cafe' exact component={Place} />
+          <Route path='/restaurant' exact component={Place} />
           <Route path='/stay' exact component={Stay} />
-          <Route path='/activity' exact component={Activity} />
+          <Route path='/stay/district' exact component={District} />
           <Route path='/plogging' exact component={Plogging} />
           <Route path='/plogging/courseView' exact component={Gpx} />
         </Switch>
