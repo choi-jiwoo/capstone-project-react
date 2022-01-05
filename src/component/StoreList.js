f8737d28aff78edcb989c56419b3598a83d457c5
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-06 02:27:28 +0900

    Rename List.js to StoreList.js

--- src/component/StoreList.js
+++ src/component/StoreList.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const List = ({ list }) => {
+const StoreList = ({ list }) => {
   const showResults = (list) =>
     list.map((item) => (
       <>
         <div
-          className='searchResultList flex justify-between text-sm pl-4 py-3 cursor-pointer hover:bg-green-100'
+          className='searchResultList flex justify-between text-sm px-2 py-3 cursor-pointer hover:bg-green-100'
           key={item.id}
         >
           <div>{item.store}</div>
           <div>
-            리뷰수 <span className='text-green-400'>{item.reviewcount}</span>
+            리뷰수 <span className='text-green-400'>{item.review_count}</span>
           </div>
         </div>
       </>
@@ -19,11 +19,11 @@ const List = ({ list }) => {
   return (
     <div
       className='grid grid-cols-1 content-center divide-y'
-      style={{ width: '320px' }}
+      style={{ width: '384px' }}
     >
       {showResults(list)}
     </div>
   );
 };
 
-export default List;
+export default StoreList;
