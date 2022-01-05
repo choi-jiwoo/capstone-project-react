bda6cb99d9573424ce579ae203f382f1cc0bcd8f
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-06 02:32:22 +0900

    Remove container style

--- src/component/Service.js
+++ src/component/Service.js
@@ -12,7 +12,7 @@ function Service() {
       <div className='mt-80 pt-4 text-center text-2xl font-extrabold'>
         공정여행 참여하기🌿
       </div>
-      <div className='container-xl flex flex-wrap flex-row justify-center items-center gap-6 pt-3 pb-7'>
+      <div className='flex flex-wrap flex-row justify-center items-center gap-6 pt-3 pb-7'>
         <Link
           className='grid border-solid border-2 border-transparent shadow-sm rounded-lg py-2 px-3'
           to='/cafe'
