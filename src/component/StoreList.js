cf980f20bfcaa365a2c940c14a72bb6167f2a0e1
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-07 00:44:16 +0900

    Add InfiniteScroll component for store search result

--- src/component/StoreList.js
+++ src/component/StoreList.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import Store from './Store';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-const StoreList = ({ list }) => {
+const StoreList = ({ data, page, setPage, hasMore, params, getRequest }) => {
+  const fetchData = () => {
+    if (page !== 1) {
+      getRequest(params, page);
+      setPage(page + 1);
+    }
+  };
 
   return (
-    <div
-      className='grid grid-cols-1 content-center divide-y'
-      style={{ width: '384px' }}
+    <InfiniteScroll
+      dataLength={data.length}
+      next={fetchData}
+      hasMore={hasMore}
+      height={512}
+      endMessage={
+        <p className='text-xs text-gray-300 text-center pb-2'>
+          더이상 로드할 수 없습니다.
+        </p>
+      }
     >
-    </div>
+      <div
+        className='grid grid-cols-1 content-center divide-y'
+        style={{ width: '384px' }}
+      >
+        {data.map((item) => (
+          <Store key={item.id} item={item} />
+        ))}
+      </div>
+    </InfiniteScroll>
   );
 };
 
