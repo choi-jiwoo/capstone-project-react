b98463dd0eb1c4202a2a4b39e95f94e331879a53
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-07 00:39:01 +0900

    Remove empty div tag when loader is null

--- src/component/Loading.js
+++ src/component/Loading.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 function Loading({ loading }) {
-  let loader = (
+  const loader = (
     <button className='w-full border-transparent p-4 mx-auto' disabled>
       <div className='animate-pulse flex space-x-4 justify-center'>
         <div className='text-blue-400 font-semibold'>Processing</div>
       </div>
     </button>
   );
-  if (!loading) loader = null;
-  return <div>{loader}</div>;
+  return <>{loading ? loader : null}</>;
 }
 
 export default Loading;
