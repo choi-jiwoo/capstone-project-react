fa31085a51506586b5585dc6cef5d8f065d4b1fc
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-10 14:00:49 +0900

    Remove keyword variables and receive it through API call

--- src/component/page/Place.js
+++ src/component/page/Place.js
@@ -16,85 +16,7 @@ function Place() {
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
-
-  // 나중에 이부분은 db에서 불러오기
-  const cafeKwrd = [
-    '가성비',
-    '강추',
-    '공간',
-    '느낌',
-    '디저트',
-    '만족',
-    '맛집',
-    '분위기',
-    '사진',
-    '서비스',
-    '선물',
-    '식빵',
-    '아이',
-    '아침',
-    '인테리어',
-    '존맛',
-    '주차',
-    '직접',
-    '초코',
-    '친절',
-    '케이크',
-    '크림',
-    '포장',
-  ];
-  const restaurantKwrd = [
-    '가성비',
-    '가족',
-    '갈치',
-    '강추',
-    '고기',
-    '고등어',
-    '구이',
-    '국물',
-    '느낌',
-    '도민',
-    '돈까스',
-    '돼지',
-    '맛집',
-    '모듬전',
-    '밑반찬',
-    '바삭',
-    '배달',
-    '백숙',
-    '볶음밥',
-    '분위기',
-    '사진',
-    '샐러드',
-    '생선',
-    '서비스',
-    '소스',
-    '수정과',
-    '야시장',
-    '양념',
-    '요리',
-    '전복',
-    '조림',
-    '존맛',
-    '중국집',
-    '짜장',
-    '짜장면',
-    '짬뽕',
-    '치킨',
-    '친절',
-    '탕수육',
-    '토종닭',
-    '튀김',
-    '파라솔',
-    '파스타',
-    '포장',
-    '포차',
-  ];
-
-  const setType = (type) => {
-    if (type === 'cafe') return cafeKwrd;
-    else return restaurantKwrd;
-  };
+  const [storeKwrd, setStoreKwrd] = useState([]);
 
   const getParameters = (checkedItems, firstPage) => {
     data.length = 0;
@@ -133,13 +55,31 @@ function Place() {
     setLoading(false);
   }, [data]);
 
+  useEffect(() => {
+    let page_type;
+    switch (type) {
+      case 'cafe':
+        page_type = 'cafe';
+        break;
+      case 'res':
+        page_type = 'restaurant';
+        break;
+      default:
+        console.log('Store type error.');
+    }
+    const url = baseUrl + page_type + '/keyword';
+    axios.get(url).then(function (response) {
+      setStoreKwrd(response.data);
+    });
+  }, []);
+
   return (
     <div className='search container-xl mt-8'>
       <div className='flex h-full relative'>
         <div className='form flex flex-col pr-5'>
           <Form
             getParameters={getParameters}
-            selectList={setType(type)}
+            selectList={storeKwrd}
             checkedItems={checkedItems}
             setCheckedItems={setCheckedItems}
           />
