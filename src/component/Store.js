9fe610eb465092bbb0a80e239bf5c731a8e9ecb0
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-24 10:13:26 +0900

    Change style of Store component

--- src/component/Store.js
+++ src/component/Store.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Store({ item }) {
   return (
-    <div className='searchResultList flex justify-between text-sm px-2 py-3 cursor-pointer hover:bg-green-100'>
-      <div>{item.store}</div>
+    <div className='searchResultList flex flex-col justify-between text-sm px-2 py-3 cursor-pointer hover:bg-green-100'>
+      <div className='text-lg'>{item.store}</div>
       <div>
         리뷰수 <span className='text-green-400'>{item.review_count}</span>
       </div>
